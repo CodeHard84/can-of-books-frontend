6fe39c44fe359d2d9e3a1853fd9bfa3ff78277f5
Jace <sockgnome@att.net>
2024-06-04 13:11:43 -0500

    Added add book button.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import AddBook from './AddBook';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {
@@ -11,26 +12,35 @@ import {
 } from "react-router-dom";
 import Profile from './About';
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
-          <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            <Route path="/about" 
-            element={<Profile />} />
-          </Routes>
-          <Footer />
-        </Router>
-      </>
-    )
-  }
+const App = () => {
+  const [showAddBookForm, setShowAddBookForm] = useState(false);
+  const [books, setBooks] = useState([]);
+
+  const updateBooks = (newBook) => {
+    setBooks([...books, newBook]);
+  };
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route 
+            exact path="/"
+            element={
+              <>
+                <BestBooks books={books} setBooks={setBooks} />
+                <button onClick={() => setShowAddBookForm(true)}>Add Book</button>
+                {showAddBookForm && <AddBook setShowAddBookForm={setShowAddBookForm} updateBooks={updateBooks} />}
+              </>
+            }
+          />
+          <Route path="/about" element={<Profile />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { Carousel } from 'react-bootstrap';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-const BestBooks = () => {
-  const [books, setBooks] = useState([]);
-
+const BestBooks = ({ books, setBooks }) => {
   useEffect(() => {
     axios.get(`${SERVER_URL}/books`)
       .then(response => {
@@ -15,7 +13,7 @@ const BestBooks = () => {
       .catch(error => {
         console.log(error);
       });
-  }, []);
+  }, [setBooks]); // <-- GPT helped here.
 
   return (
     <>
