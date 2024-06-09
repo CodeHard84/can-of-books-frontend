8d2f797adf0e83062212ca4beceea463df9724ab
Jace <sockgnome@att.net>
2024-06-08 22:43:00 -0500

    Feature: add book editing functionality

--- src/BookFormModal.jsx
+++ src/BookFormModal.jsx
@@ -1,34 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
-const BookFormModal = ({ show, handleClose, updateBooks }) => {
+const BookFormModal = ({ show, handleClose, updateBooks, bookToEdit }) => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [status, setStatus] = useState('');
 
+  useEffect(() => {
+    if (bookToEdit) {
+      setTitle(bookToEdit.title);
+      setDescription(bookToEdit.description);
+      setStatus(bookToEdit.status);
+    } else {
+      setTitle('');
+      setDescription('');
+      setStatus('');
+    }
+  }, [bookToEdit]);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await axios.post(`${SERVER_URL}/books`, {
-        title,
-        description,
-        status
-      });
-      console.log('Book added:', response.data);
-      updateBooks(response.data);
+      if (bookToEdit) {
+        const response = await axios.put(`${SERVER_URL}/books/${bookToEdit._id}`, {
+          title,
+          description,
+          status
+        });
+        console.log('Book updated:', response.data);
+        updateBooks(response.data);
+      } else {
+        const response = await axios.post(`${SERVER_URL}/books`, {
+          title,
+          description,
+          status
+        });
+        console.log('Book added:', response.data);
+        updateBooks(response.data);
+      }
       handleClose();
     } catch (error) {
-      console.error('Error adding book:', error);
+      console.error('Error saving book:', error);
     }
   };
 
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>Add New Book</Modal.Title>
+        <Modal.Title>{bookToEdit ? 'Edit Book' : 'Add New Book'}</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form onSubmit={handleSubmit}>
@@ -63,13 +85,14 @@ const BookFormModal = ({ show, handleClose, updateBooks }) => {
               <option value="reserved">Reserved</option>
             </Form.Select>
           </Form.Group>
+          {/* Finally, figured it out! */}
           <Button variant="primary" type="submit" style={{ marginTop: '10px' }}>
-            Add Book
+            {bookToEdit ? 'Update Book' : 'Add Book'}
           </Button>
         </Form>
       </Modal.Body>
     </Modal>
   );
-}
+};
 
 export default BookFormModal;
